b9b0543926f4a01975a8da2cdf48c821c6cb4b60
zZz <freedom.xser@gmail.com>
2024-10-09 10:14:51 +0630

    git test conflict

--- src/App.js
+++ src/App.js
@@ -1,52 +1,50 @@
 import React, { useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
-import BookEntry from "./pages/book/Entry";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 
-import BookList from "./pages/book/List";
-import UserList from "./pages/user/List";
+import BookEntry from './pages/book/Entry';
+import BookList from './pages/book/List';
 
-import Login from "./pages/auth/Login";
-import Entry from "./pages/auth/Entry";
+import LibrarianList from './pages/user/List';
+
+import Login from './pages/auth/Login';
+import Entry from './pages/auth/Entry';
+
 
 // import useAxiosInterceptor from "./helper/axiosInterceptor";
 
 import RouteGuard from "./pages/auth/RouteGuard";
 
 function App() {
-  // useAxiosInterceptor();
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<RedirectToMainPage />} />
-        <Route path="*" element={<RedirectToMainPage />} />
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/register" element={<Entry />} />
+    // useAxiosInterceptor()
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<RedirectToMainPage />} />
+                <Route path="*" element={<RedirectToMainPage />} />
+                <Route path="/auth/login" element={<Login/>}/>
+                <Route path="/auth/register" element={<Entry/>}/>
 
-        {/* Route Guard */}
-        <Route element={<RouteGuard allowedRoles={["Admin", "Staff"]} />}>
-          <Route path="/users/list" element={<UserList />} />
-        </Route>
+                {/* Route Guard */}
+                <Route element={<RouteGuard allowedRoles={["Admin","Staff"]}/>}>
+                    <Route path="/users/list" element={<LibrarianList/>}/>
+                </Route>
 
-        <Route path="/products/list" element={<BookList />} />
-        <Route path="/products/entry" element={<BookEntry />} />
-        <Route path="/products/edit/:id" element={<BookEntry />} />
-      </Routes>
-    </Router>
-  );
+                <Route path="/books/list" element={<BookList/>}/>
+                <Route path="/books/entry" element={<BookEntry/>}/>
+                <Route path="/books/edit/:id" element={<BookEntry/>}/>
+
+            </Routes>
+        </Router>
+    );
 }
 
 function RedirectToMainPage() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate("/auth/login");
-  }, [navigate]);
+    const navigate = useNavigate();
+    useEffect(()=>{
+        navigate("/auth/login");
+    },[navigate])
 
-  return null;
+    return null;
 }
 
 export default App;
